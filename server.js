7feceb1fe9ea7e0bedd9fa90a61cf48f9587dd66
Pranay Khadse <pranitkhadse20@gmail.com>
2023-04-17 22:37:13 +0530

    add

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const jsonServer = require('json-server')
+const cors = require('cors')
+const server = jsonServer.create()
+const Router = jsonServer.router('./db.json')
+const middleware = jsonServer.defaults()
+const port = 8000
 
 server.use(cors())
 server.use(Router)
